83b908919f0c4f8c4ee2bfd81dc2f05d747dbd38
Keizo-Ito <keizo.ito@r5.dion.ne.jp>
2024-05-17 18:25:37 +0900

    Initial Commit

---
+++ Sources/MyStringPackage/MyStringPackage.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct MyFirstSwiftPackage 
+{
+    public private(set) var text = "Greeting from MyStringPackage!"
+    
+    public init () {
+    }
+}
